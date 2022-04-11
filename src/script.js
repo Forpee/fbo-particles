b401f8515ff08452b86217efccca689272cd4614
Forpee <khalilhassam3@gmail.com>
2022-04-11 16:30:24 +0200

    update

--- src/script.js
+++ src/script.js
@@ -86,7 +86,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0.25, - 0.25, 1)
+camera.position.set(0.0, - 0.0, 2)
 scene.add(camera)
 
 // Controls
